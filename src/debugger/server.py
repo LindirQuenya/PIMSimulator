713c6a8e27eb3ce4727cee01d32bc37ab1847b71
N-K-22 <khamarnitya@gmail.com>
2025-04-27 23:37:16 -0400

    added in code to start basic communication with the GDB server protocol. Next step is to verify the output behaves properly otherwise debug

--- src/debugger/server.py
+++ src/debugger/server.py
@@ -1,14 +1,25 @@
 import socket
 import signal #for hanlding an interrupt to end gdb server
 
-
-class GDB(object):
+class GDB_SERVER(object):
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        port = input("GDB Port:")
+        port = input("GDB Socket Port Target:")
         port = int(port)
+        self.sock.setsockopt(level=socket.SOL_SOCKET, option_name=socket.SO_KEEPALIVE)
         self.sock.settimeout(15)
         self.sock.bind((socket.gethostbyaddr(), port)) #used socket documentation
+        #self.data = " "
+    def receiver(self):
+        data = self.data
+        try:
+            data = self.sock.recv()
+        except self.sock.timeout:
+            pass
+        else: 
+            if data == " " or data == b'':
+                raise RuntimeError("No data is being received, Something went wrong --> GDB server not sending constant stream of message")
+        print(F"Data received: {data}")    
 
 
     def close_socket(self):
